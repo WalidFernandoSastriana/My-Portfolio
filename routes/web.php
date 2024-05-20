722eb876b41377df1b0196fe0e1d9bca0ffbe473
Walid Fernando Sastriana <142325312+WalidFernandoSastriana@users.noreply.github.com>
2024-05-20 12:48:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('index');
 });
+
+Route::get('/cv', function () {
+    return view('cv');
+});
+
+Route::get('/experience', function () {
+    return view('experience');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/', [HomeController::class, 'index']);
